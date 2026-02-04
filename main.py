3d2635ef5a94d0e1b9572d13770b400befb211b8
wqrvQ2WR <nyeoni211@gmail.com>
2026-02-04 15:09:15 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import webbrowser
-import random
+set_id = input("아이디 설정: ")
+set_pw = input("비밀번호 설정: ")
 
-weblist = ["google.com","youtube.com","facebook.com","instagram.com","chatgpt.com","wikipedia.org","bing.com","yahoo.com","amazon.com","netflix.com","microsoft.com","naver.com","namu.wiki"]
-
-webbrowser.open(random.choice(weblist))
+while True:
+    id = input("아이디 입력: ")
+    pw = input("비밀번호 입력: ")
+    if id == set_id and pw == set_pw:
+        print("아이디,비밀번호 일치")
+        print("접근을 허용합니다")
+    else:
+        print("아이디나 비밀번호가 틀렸습니다")
